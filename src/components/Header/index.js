df74672128a47528229029b472d93ba84d265cff
Arief R Ramadhan <arief.rizky@doku.com>
2021-05-01 12:32:26 +0700

    Update blog URL

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -15,12 +15,12 @@ function HeaderComponent () {
                     <div className="hidden md:flex items-center space-x-1">
                         <Link to="/" className="px-3 text-gray-700 hover:text-gray-900">Home</Link>
                         <Link to="/about" className="px-3 text-gray-700 hover:text-gray-900">About</Link>
-                        <Link className="px-3 text-gray-700 hover:text-gray-900">Blog</Link>
+                        <Link to={{ pathname: 'https://medium.com/@ariefrizkyr' }} target="_blank" className="px-3 text-gray-700 hover:text-gray-900">Blog</Link>
                     </div>
                 </div>
 
                 <div className="md:hidden flex justify-between items-center">
-                    <span className="text-gray-800 font-bold">Arief R Ramadhan</span>
+                    <Link to="/" className="text-gray-800 font-bold">Arief R Ramadhan</Link>
                     <button onClick={openToggle}>
                         <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -32,7 +32,7 @@ function HeaderComponent () {
             <div className={`${!open && 'hidden'} md:hidden`} >
                 <Link to="/" onClick={openToggle} className="block py-2 px-4 text-sm hover:bg-gray-200">Home</Link>
                 <Link to="/about" onClick={openToggle} className="block py-2 px-4 text-sm hover:bg-gray-200">About</Link>
-                <Link className="block py-2 px-4 text-sm hover:bg-gray-200">Blog</Link>
+                <Link to={{ pathname: 'https://medium.com/@ariefrizkyr' }} target="_blank"  className="block py-2 px-4 text-sm hover:bg-gray-200">Blog</Link>
             </div>
         </nav>
     );
